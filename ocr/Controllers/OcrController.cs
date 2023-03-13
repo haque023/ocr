8f7f03e31ce8207be4a555151779efbaf87f0acb
Emdadul Haque <Emdad-Pc@DESKTOP-0SM6RSD>
2023-03-13 15:36:32 +0600

    OcrGenerate ready from url

--- ocr/Controllers/OcrController.cs
+++ ocr/Controllers/OcrController.cs
@@ -7,45 +7,46 @@ using Tesseract;
 
 namespace ocr.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("ocr/[controller]")]
     [ApiController]
     public class OcrController : ControllerBase
     {
         public const string folderName = "Imgaes/";
         public const string trainedDataFolderName = "tdata";
         [HttpPost]
+        [Route("OcrGenerate")]
         public String DoOCR()
         {
             string result = "";
 
-            for (var i = 0; i < 10; i++)
+
+            using var wc = new System.Net.WebClient();
+
+            var pdfUrl = "https://www.africau.edu/images/default/sample.pdf";
+            wc.DownloadFile(pdfUrl, "output.pdf");
+            wc.Dispose();
+
+            var document = new Document("output.pdf");
+            var renderer = new Aspose.Pdf.Devices.PngDevice();
+
+            string tessPath = Path.Combine(trainedDataFolderName, "");
+            using (var engine = new TesseractEngine(tessPath, "eng", EngineMode.Default))
             {
-                using var wc = new System.Net.WebClient();
-                wc.DownloadFile("https://www.africau.edu/images/default/sample.pdf", "output.pdf");
-                wc.Dispose();
 
-                var document = new Document("output.pdf");
-                var renderer = new Aspose.Pdf.Devices.PngDevice();
-
-                string tessPath = Path.Combine(trainedDataFolderName, "");
-                using (var engine = new TesseractEngine(tessPath, "eng", EngineMode.Default))
+                foreach (var itm in document.Pages)
                 {
-
-                    foreach (var itm in document.Pages)
+                    renderer.Process(itm, "output.png");
+                    var imageFileName = "output.png";
+                    using (var img = Pix.LoadFromFile(imageFileName))
                     {
-                        renderer.Process(itm, "output.png");
-                        var imageFileName = "output.png";
-                        using (var img = Pix.LoadFromFile(imageFileName))
-                        {
-                            var page = engine.Process(img);
-                            result += page.GetText();
-                            page.Dispose();
-                            Console.WriteLine(result);
-                        }
+                        var page = engine.Process(img);
+                        result += page.GetText();
+                        page.Dispose();
                     }
-
                 }
+
             }
+
             return String.IsNullOrWhiteSpace(result) ? "Ocr is finished. Return empty" : result;
         }
     }
